b0e05f6ac44884411051185f1c070d57bf04c430
edmayen <ed_mayen@hotmail.com>
2019-09-02 17:54:05 -0500

    version 1.0.4

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,10 +10,16 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) 
   {
-    //console.log('Servicio de infoPagina listo');
-    
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo()
+  {
     //leer el archivo JSON
 
     this.http.get('assets/data/data-pagina.json')
@@ -21,8 +27,20 @@ export class InfoPaginaService {
 
           this.cargada = true;
           this.info = resp;
-          console.log(resp);
+        });
+  }
+
+  private cargarEquipo()
+  {
+    //leer el archivo JSON
+
+    this.http.get('https://angular-html-cfe81.firebaseio.com/equipo.json')
+        .subscribe( (resp: any[]) => {
+
+          this.equipo = resp;
+          //console.log(resp);
 
         });
   }
+
 }
